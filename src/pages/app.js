645c8816f372f65dc57839416e9c95d58c905343
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-21 13:22:59 +0630

    add basepath in router

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,15 +1,15 @@
 import React from "react"
 import { Router } from "@reach/router"
+import Login from "../components/login"
 import Layout from "../components/layout"
 import Profile from "../components/profile"
-import Login from "../components/login"
 import PrivateRoute from "../components/privateRoute"
 
 const App = () => (
   <Layout>
-    <Router>
-      <PrivateRoute path="/app/profile" component={Profile} />
-      <Login path="/app/login" />
+    <Router basepath="/app">
+      <PrivateRoute path="/profile" component={Profile} />
+      <Login path="/login" />
     </Router>
   </Layout>
 )
